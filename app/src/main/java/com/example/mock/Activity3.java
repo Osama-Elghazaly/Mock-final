a5cd8533b5eb23415f3c1231b61f477be49b1551
Osama-Elghazaly <oaelghazaly@alfaisal.edu>
2021-03-03 14:28:28 +0300

    Mock

---
+++ app/src/main/java/com/example/mock/Activity3.java
@@ -0,0 +1,52 @@
+package com.example.mock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Activity3 extends ListActivity {
+
+    String[] items={"Option 1","Option 2","Option 3","Option 4","Option 5"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+        setListAdapter(new ArrayAdapter<String>(this,R.layout.activity_3,R.id.txtViewList,items) );
+
+
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        switch(position){
+
+            case 1:
+                startActivity(new Intent(Activity3.this,showBig.class));
+                break;
+            case 2:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://chocolatharlem.com/")));
+                break;
+            case 3:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com")));
+                break;
+            case 4:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://facebook.com")));
+                break;
+            case 5:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtube.com")));
+                break;
+
+
+        }
+    }
+}
